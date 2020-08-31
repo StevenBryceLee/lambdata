c42a15294db0a3a3fab8e0d732067daa6637545d
StevenBryceLee <stevenl451@gmail.com>
2020-08-31 10:38:25 -0700

    adding setup.py

--- lambdata/__init__.py
+++ lambdata/__init__.py
@@ -1,1 +1,10 @@
+"""lambdata - a collection of 
+Data Science helper functions
+"""
+import pandas as pd
+import numpy as np
+
+Test = pd.DataFrame(np.ones(10))
+
+
 print('hello world')
---
+++ setup.py
@@ -0,0 +1,31 @@
+"""lambdata - a collection of helper functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open('README.md','r') as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name='lambdata-StevenBryceLee',
+    version = '0.0.1',
+    author = 'StevenBryceLee',
+    description = 'A collection of helper functions',
+    long_description = LONG_DESCRIPTION,
+    long_description_content = 'text/markdown',
+    url = 'https://github.com/StevenBryceLee/lambdata/',
+    packages = setuptools.find_packages(),
+    python_requires = '>=3.6',
+    install_requires = REQUIRED,
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operationg System :: OS Independent',
+        
+    ]
+)
